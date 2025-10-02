559e903aa7e6329bfd2f16d56848240cd9915707
pb-06 <kkandomail@gmail.com>
2025-10-02 09:43:54 +0200

    Implement handleLogClick()

--- react-app/src/FunctionComponent.jsx
+++ react-app/src/FunctionComponent.jsx
@@ -1,7 +1,18 @@
 export default function FunctionComponent(props) {
-    <div className="card">
-        <p><button>Increase Number</button></p>
-        <p>Number: {0}</p>
-        <p><button>Log props, state</button></p>
-    </div>
+    const handleLogClick = () => {
+        try {
+            console.log('handleLogClick props', props);
+            console.log('handleLogClick state', state);
+        } catch (e) {
+            console.warn(e);
+        }
+    }
+
+    return (
+        <div className="card">
+            <p><button>Increase Number</button></p>
+            <p>Number: {0}</p>
+            <p><button onClick={handleLogClick}>Log props, state</button></p>
+        </div>
+    )
 }
