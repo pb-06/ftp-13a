861b9185f838253007b6a188679dca85d9982707
pb-06 <kkandomail@gmail.com>
2025-10-02 08:43:24 +0200

    Refactor GET ftp-list

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -2,24 +2,37 @@ const express = require('express');
 const { Client } = require('basic-ftp');
 const app = express();
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.sendStatus(200);
 });
 
-app.get('/ftp-list', (req, res)=>{
-    const ftpClient = new Client();
-    ftpClient.access({
-        host: '192.168.56.1',
-        port: '21',
-        user: 'tester',
-        password: 'password',
-    })
-    .then(console.log)
-    .catch(console.warn)
-    .finally(()=>res.sendStatus(200))
+app.get('/ftp-list', async (req, res) => {
+    let statusCode = 200;
+    let responseBody = {
+        data: [],
+        error: null
+    }
+    try {
+        const ftpClient = new Client();
+        await ftpClient.access({
+            host: '192.168.56.1',
+            port: '21',
+            user: 'tester',
+            password: 'password',
+        })
+        await ftpClient.cd('/');
+        const fileList = await ftpClient.list();
+        ftpClient.close();
+        responseBody.data = fileList;
+    } catch(e) {
+        statusCode = 404;
+        responseBody.error = e;
+    }
+   
+    res.status(statusCode).json(responseBody);
 });
 
 const port = 3333;
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Backend server starts on port', port);
 })
