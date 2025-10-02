267f517abb0f891aaf505465c23886a293f9bf27
pb-06 <kkandomail@gmail.com>
2025-10-02 10:26:06 +0200

    Implement componentDidUnmount()

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -5,14 +5,22 @@ import './App.css'
 import FunctionComponent from './FunctionComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [showChild, setShowChild] = useState(true);
+
+  const handleToggleChildClick = () => {
+    setShowChild(!showChild);
+  }
 
   return (
     <>
       <h1>Parent, but this is my child:</h1>
-      <FunctionComponent fruit="apple" callback={console.log}>
-        <p>child paragraph</p>
-      </FunctionComponent>
+      <p><button onClick={handleToggleChildClick}>Toggle child existence</button></p>
+      {showChild &&
+        <FunctionComponent fruit="apple" callback={console.log}>
+          <p>child paragraph</p>
+        </FunctionComponent>
+      }
     </>
   )
 }
--- react-app/src/FunctionComponent.jsx
+++ react-app/src/FunctionComponent.jsx
@@ -28,6 +28,7 @@ export default function FunctionComponent(props) {
 
     useEffect(() => {
         componentDidMount();
+        return componentDidUnMount
     }, [])
 
     return (
