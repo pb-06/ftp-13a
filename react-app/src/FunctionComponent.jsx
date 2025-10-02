555beff552bb003a9e73654d996c628049ac9bb9
pb-06 <kkandomail@gmail.com>
2025-10-02 09:41:01 +0200

    Add function

---
+++ react-app/src/FunctionComponent.jsx
@@ -0,0 +1,7 @@
+export default function FunctionComponent(props) {
+    <div className="card">
+        <p><button>Increase Number</button></p>
+        <p>Number: {0}</p>
+        <p><button>Log props, state</button></p>
+    </div>
+}
